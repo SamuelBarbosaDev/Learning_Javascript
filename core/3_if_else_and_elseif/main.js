e00d535751a151633207bc278b9d98c8bffd8c46
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-08-26 13:08:00 -0300

    feat: Estudo sobre condicionais em javascript

---
+++ core/3_if_else_and_elseif/main.js
@@ -0,0 +1,15 @@
+/// Qual é a sua idade?
+
+let year_old = 10000;
+
+if (year_old === 20) {
+    console.log("Você tem 20 anos.");
+}
+
+else if (year_old === 21) {
+    console.log("Você tem 21 anos.");
+}
+
+else {
+    console.log("Sua idade não esperada!!!")
+}
