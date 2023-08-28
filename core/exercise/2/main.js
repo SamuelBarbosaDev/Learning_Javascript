90cb8e6ef720ff1c62fe2671f187b19c069b00ad
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-08-28 10:27:52 -0300

    feat: Exercício, calculando o IMC

---
+++ core/exercise/2/main.js
@@ -0,0 +1,12 @@
+/*
+* Como calcular o IMC? 
+*/
+
+// Suas médidas:
+let altura = 1.75;
+let peso = 80;
+
+// Calculo do índice de massa corporal:
+let imc = peso / (altura * altura);
+
+console.log(`O seu IMC é: ${imc}`)
