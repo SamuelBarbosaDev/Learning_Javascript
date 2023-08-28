62d62b0d7224a014e5e840ca6c7f6f39532fc153
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-08-28 07:45:34 -0300

    chore: Criando a pasta exercise para conter exercício de javascript

---
+++ core/exercise/1/main.js
@@ -0,0 +1,8 @@
+/*
+* Como imprimir a mensagem abaixo, separando as letras e símbolos com string e os números como number?
+* Estou aprendendo JavaScript às 7 da manhã.
+*/
+
+let hora = 7;
+
+console.log('Estou aprendendo JavaScript às', hora, 'da manhã.')
