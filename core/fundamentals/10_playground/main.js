937cf20fe031e71bb414d6a58d1e8c360b00a876
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-08-29 10:00:53 -0300

    feat: Estudando o comportamento do NaN

--- core/fundamentals/10_playground/main.js
+++ core/fundamentals/10_playground/main.js
@@ -1,5 +1,16 @@
+// O que acontece se somamos um número com uma string?
 let num1 = '5';
 let num2 = 5;
-let result = num1 + num2;
+let result_task_1 = num1 + num2;
 
-console.log(typeof result, result)
+console.log(`Resultado da task 1: ${typeof result_task_1, result_task_1}`);
+
+// O que acontece se convertemos uma string em um número?
+let nome = Number('Samuel');
+
+console.log(`Resultado da task 2: ${nome}`)
+
+// O que acontece se somamos a string que convertemos em um número?
+let result_task_3 = nome + 20;
+
+console.log(`Resultado da task 3: ${result_task_3}`)
