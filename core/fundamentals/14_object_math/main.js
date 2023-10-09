3ca4de208c24715b6b7a88d2577f58e7e44f93a4
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-10-09 09:15:52 -0300

    feat: Estudo sobre o object math em javascript

---
+++ core/fundamentals/14_object_math/main.js
@@ -0,0 +1,15 @@
+let num_1 = 10.69;
+let num_2 = Math.round(num_1);
+let max = Math.max(3, 3, 4, 5, 66, 7, 99, 54, 34, 7, 42, 2)
+let min = Math.min(3, 3, 4, 5, 66, 7, 99, 54, 34, 7, 42, 2)
+let aleatorio = Math.random()
+let potencia = Math.pow(2, 8)
+let infinit = 100 / 0
+
+
+console.log(`Arredondando: ${num_2}`)
+console.log(`Max: ${max}`)
+console.log(`Min: ${min}`)
+console.log(`Aleatório: ${aleatorio}`)
+console.log(`Potência: ${potencia}`)
+console.log(`Infinito: ${infinit}`)
