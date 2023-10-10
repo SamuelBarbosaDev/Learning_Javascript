e759b52fd1dd198c327a3c27103cd77f2d4cf08f
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-10-10 09:47:31 -0300

    feat: Estudo sobre operadores lógicos e comparação em javascript

--- core/fundamentals/7_logical_operators/main.js
+++ core/fundamentals/7_logical_operators/main.js
@@ -1,7 +1,35 @@
-let chove = false;
+/*
+Operadores de comparação:
 
-if (!chove) {
-  console.log("Não está chovendo.");
-} else {
-  console.log("Está chovendo.");
-}
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) **Não usar**
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **Não usar**
+!== diferente estrito (valor e tipo)
+*/
+
+/*
+Operadores Lógicos:
+
+&& -> and -> E
+|| -> or -> ou
+! -> not -> não 
+*/
+
+const chove = true;
+const dia_bonito = true;
+
+if (chove || dia_bonito)
+    console.log("Eu gosto de chuva ou de sol?")
+
+else if (!chove && dia_bonito)
+    console.log("Não está chovendo, e o dia está bonito.");
+
+else if (!chove)
+    console.log("Não está chovendo.");
+
+else
+    console.log("Está chovendo.");
