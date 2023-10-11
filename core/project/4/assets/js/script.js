cbd2f89f1645e4319625890b9bd7a11c30f4b658
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-10-11 09:01:36 -0300

    feat: Atualizando projeto 4, calculadora de IMC

--- core/project/4/assets/js/script.js
+++ core/project/4/assets/js/script.js
@@ -1,12 +1,37 @@
 function form() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
 
     function recebeEventoForm(event) {
         event.preventDefault();
         const peso = form.querySelector('#peso');
         const altura = form.querySelector('#altura');
         const imc = peso.value / (altura.value * altura.value)
-        alert(`O seu IMC é: ${imc.toFixed(1)}`);
+        let status_imc = NaN
+
+        if (imc < 18.5){
+            status_imc = 'Abaixo do peso'
+        }
+        else if ((imc > 18.5) && (imc <= 24.0)){
+            status_imc = 'Peso normal'
+        }
+        else if ((imc >= 25) && (imc <= 29.9)){
+            status_imc = 'Sobrepeso'
+        }
+        else if ((imc >= 30) && (imc <= 34.9)){
+            status_imc = 'Obesidade grau 1'
+        }
+        else if ((imc >= 35) && (imc <= 39.9)){
+            status_imc = 'Obesidade grau 2'
+        }
+        else if (imc > 40){
+            status_imc = 'Obesidade grau 3'
+        }
+        else {
+            status_imc = 'Desconhecido'
+        }
+        
+        resultado.innerHTML = `<p>O seu IMC é: <strong>${imc.toFixed(1)}</strong> - <strong>${status_imc}</strong></p>`
     };
 
     form.addEventListener(
