213d976d8f4f0d3a4fd88a4d92ea37b4e4476f50
Samuel <samueloficial@protonmail.com>
2024-01-09 09:22:38 -0300

    feat: Estudo sobre `continue` & `braek`

---
+++ core/notes/10/main.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let number of numbers){
+    if (number === 2){
+        continue;
+    }
+
+    else if(number === 8){
+        break;
+    }
+
+    console.log(number)
+}
