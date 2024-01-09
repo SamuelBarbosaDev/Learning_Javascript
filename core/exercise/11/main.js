3e9e90b3f08d8fd23e5c961fe8d86adc4f996f70
Samuel <samueloficial@protonmail.com>
2024-01-09 10:51:11 -0300

    feat: Execício do `FizzBuzz`

---
+++ core/exercise/11/main.js
@@ -0,0 +1,11 @@
+function fizzBuzz(num){
+    if (typeof num !== 'number') return num;
+    else if ((num % 3 && num % 5) === 0) return 'FizzBuzz';
+    else if (num % 3 === 0) return 'Fizz';
+    else if (num % 5 === 0) return 'Buzz';
+    return num
+};
+
+for (let i = 0; i <= 100; i++){
+    console.log(fizzBuzz(i))
+}
