77fb2b82ce811d55745e584c56f54923616223a5
Samuel <samueloficial@protonmail.com>
2024-01-22 09:36:13 -0300

    feat: Estudo sobre `recursividade` parte 2

---
+++ core/notes/22/main.js
@@ -0,0 +1,15 @@
+// Recursividade:
+function multiplicar (var_1, var_2){
+    if (var_1 === 0 || var_2 === 0){
+        return 0
+    }
+
+    else if (var_2 === 1){
+        return var_1
+    }
+
+    else {
+        return var_1 + (multiplicar(var_1, var_2 - 1))
+    }
+}
+console.log(multiplicar(2, 1))
