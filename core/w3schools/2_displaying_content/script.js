cd60483d951afc7635392059d6eb5d7bac14e2e2
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-06-10 14:22:25 -0300

    feat: Estudo sobre a exibição de conteúdo

---
+++ core/w3schools/2_displaying_content/script.js
@@ -0,0 +1,9 @@
+// Variáveis
+const aviso = "==== AVISO! ====";
+const subtitle = document.querySelector("span")
+
+// Exibindo conteúdo
+window.alert(aviso)
+console.log("Nome")
+document.write("<p>Escrevendo no HTML.</p>")
+subtitle.innerHTML += "Subtitulo."
