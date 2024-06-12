06c252a8a325ed38d4e868fd47551b05cf30c78a
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-06-12 10:59:29 -0300

    feat: Estudo sobre atribuição

---
+++ core/w3schools/5_assignment_operators/script.js
@@ -0,0 +1,123 @@
+// Atribuição de Adição
+let add = 10;
+add += 5;
+
+console.log(add)
+
+// Atribuição de Subtração
+let sub = 100;
+sub -= 5;
+
+console.log(sub);
+
+// Atribuição de Multiplicação
+let mult = 10;
+mult *= 5;
+
+console.log(mult)
+
+// Atribuição de Divisão
+let div = 10;
+div /= 5;
+
+console.log(div)
+
+// Atribuição de Resto da Divisão
+let rd = 10;
+rd %= 5;
+
+console.log(rd)
+
+// Atribuição de deslocamento a Esquerda
+let de = -100;
+de <<= 1;
+
+console.log(de)
+
+// Atribuição de deslocamento a Direita
+let dd = -100;
+dd >>>= 1;
+
+console.log(dd)
+
+// Atribuição de deslocamento muito a Direita 
+let dmd = -100;
+dmd >>>= 1;
+
+console.log(dmd)
+
+// AND Bit a Bit
+let aa = 10;
+aa &= 5;
+
+console.log(aa)
+
+// XOR Bit a Bit
+let x = 10;
+x ^= 5;
+
+console.log(x)
+
+// OR Bit a Bit
+let o = 10;
+o |= 5;
+
+console.log(o)
+
+//
+let a = 10;
+a &&= 5;
+
+console.log(a)
+
+//
+let bb = 10;
+bb ||= 5;
+
+console.log(bb)
+
+//
+let cc;
+cc ??= 5;
+
+console.log(cc)
+/*
+Operadores de atribuição de JavaScript
+
+Os operadores de atribuição atribuem valores a variáveis JavaScript.
+Operator 	Example 	Same As
+= 	        x = y 	    x = y
++= 	        x += y 	    x = x + y
+-= 	        x -= y 	    x = x - y
+*= 	        x *= y 	    x = x * y
+/= 	        x /= y 	    x = x / y
+%= 	        x %= y 	    x = x % y
+**= 	    x **= y 	x = x ** y
+
+
+
+
+Operadores de Atribuição de Desloque
+Operator 	Example 	Same As
+<<= 	    x <<= y 	x = x << y
+>>= 	    x >>= y 	x = x >> y
+>>>= 	    x >>>= y 	x = x >>> y
+
+
+
+
+Operadores de Atribuição Bitwise
+Operator 	Example 	Same As
+&= 	        x &= y 	    x = x & y
+^= 	        x ^= y 	    x = x ^ y
+|= 	        x |= y 	    x = x | y
+
+
+
+
+Operadores de Atribuição Lógica
+Operator 	Example 	Same As
+&&= 	    x &&= y 	x = x && (x = y)
+||= 	    x ||= y 	x = x || (x = y)
+??= 	    x ??= y 	x = x ?? (x = y)
+*/
