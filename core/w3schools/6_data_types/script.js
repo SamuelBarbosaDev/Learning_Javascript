41e49c0ee33993e2bc7c4301b29c5655357c4d44
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-06-12 16:29:30 -0300

    feat: Estudo sobre tipo de dados

---
+++ core/w3schools/6_data_types/script.js
@@ -0,0 +1,27 @@
+// Tipos de Dados
+
+// Numbers:
+let idade = 16;
+let altura = 1.83;
+
+// Strings:
+let cor = "Azul";
+let primeiroNome = "Fulano";
+
+// Booleans:
+let x = true;
+let y = false;
+
+// Object:
+const pessoa = {
+    primeiroNome: "Fulano",
+    segundoNome: "De tal"
+};
+
+// Array object:
+const carros = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const data = new Date("2025-01-27")
+
+console.log(data)
