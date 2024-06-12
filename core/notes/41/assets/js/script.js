84ab91bce10ba049f627d4dcbb1a70a1ee65b06a
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-06-12 13:22:49 -0300

    feat: Lembrando como mudar a cor dos objetos

---
+++ core/notes/41/assets/js/script.js
@@ -0,0 +1,9 @@
+const quadradoColorido = document.querySelector("#quadrado-colorido")
+
+// quadradoColorido.addEventListener(
+//     "click", function(){
+//         quadradoColorido.style.backgroundColor = 'lightgreen'
+//     }
+// )
+
+quadradoColorido.style.backgroundColor = "red"
