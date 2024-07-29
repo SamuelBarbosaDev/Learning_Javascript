1cf2489477245686425593ca8a9824b3d7d5163c
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-07-29 17:03:16 -0300

    feat: Estudo sobre node - `Modules`

---
+++ core/node/1_modules/matematica.js
@@ -0,0 +1,17 @@
+function soma(x, y){
+    return x + y;
+};
+
+function subtração(x, y){
+    return x - y;
+};
+
+function divisão(x, y){
+    return x / y;
+};
+
+exports.soma = soma;
+module.exports.subtração = subtração;
+this.divisão = divisão;
+
+exports.qualquerCoisa ='dfsadfsdafsd'
---
+++ core/node/1_modules/script.js
@@ -0,0 +1,7 @@
+const {soma} = require('./matematica.js');
+const kkk = require('./matematica.js');
+const {divisão, subtração} = require('./matematica.js')
+
+
+console.log(soma(4, 1))
+console.log(kkk.qualquerCoisa)
