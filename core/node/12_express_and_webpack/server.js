8c3a365e6abf2a45dd6e8a9b0b4849dd3b95901c
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-08-01 15:59:47 -0300

    feat: Estudo sobre `express + webpack`

---
+++ core/node/12_express_and_webpack/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require('./router');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(router);
+
+app.listen(3999,
+    () => {
+        console.log('Acesse o site: http://localhost:3999/')
+    }
+);
