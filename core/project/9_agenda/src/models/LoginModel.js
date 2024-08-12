30098f184595b7df0982d5e7e6feb08b31e69fe5
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-08-12 19:04:46 -0300

    feat: Atualizando `LoginModel`
    - Transformando a senha em hash com bcryptjs.
    - Adicionando a função userExists para verificar se o e-mail já está cadastrado na base de dados.

--- core/project/9_agenda/src/models/LoginModel.js
+++ core/project/9_agenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
     email: {type: String, required: true},
@@ -17,13 +18,17 @@ class Login{
 
     async register(){
         this.valida();
+        await this.userExists();
         if(this.errors.length > 0) return;
-        try{
-            this.user = await LoginModel.create(this.body);
-        }
-        catch(e){
-            console.log(e);
-        }
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+        this.user = await LoginModel.create(this.body);
+
+    }
+
+    async userExists(){
+        this.user = await LoginModel.findOne({email: this.body.email});
+        if(this.user) this.errors.push('Usuário já existe.');
     }
 
     valida() {
